e4e73bea62c03709dd5554ff96d800f8121926cb
Zin Ko <zinko@Zins-MacBook-Air.local>
2024-11-12 10:20:09 +0900

    Edit2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./Components/Header";
+import Hero from "./Components/Hero";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
+    <div className="min-h-screen bg-gradient-to-r from-[#60072C] via-[#120B2E] to-[#091498] px-8 md:px-14 lg:px-36 pb-10 pt-10">
       <Header />
+
+      <Hero />
     </div>
   );
 }
--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { HiMiniBars3BottomLeft } from "react-icons/hi2";
+import { HiXMark } from "react-icons/hi2";
 
 function Header() {
+  const [toggle, settoggle] = usestate(false);
   const menuList = [
     {
       id: 1,
@@ -23,20 +26,39 @@ function Header() {
       title: "CONTENT",
     },
   ];
+
   return (
-    <div>
+    <div className="flex items- justify-between">
       <div>
         <h1 className="text-[24px] text-white font-bold ">
           ZINKO <span className="text-red-500">NAING</span>
         </h1>
       </div>
-      <div>
+      <div className=" hidden md:flex gap-4">
         {menuList.map((item) => (
           <div>
-            <h2>{item.title}</h2>
+            <h2 className="text-whiter text-[15px] hover:border-[1px] border-red-500 rounded-full px-3 py-1 cursor-pointer">
+              {item.title}
+            </h2>
           </div>
         ))}
+        <h2 className="text-whiter text-[15px] hover:border-[1px] border-red-500 rounded-full hover:bg-gradient-to-r from-red-500 to-red-800 px-3 py-1 cursor-pointer">
+          Hire me
+        </h2>
       </div>
+      {/* <div className="md:hidden">
+        {!toggle ? (
+          <HiMiniBars3BottomLeft
+            onClick={() => settoggle(!toggle)}
+            className="text-white text-[22px]"
+          />
+        ) : (
+          <HiXMark
+            onClick={() => settoggle(!toggle)}
+            className="text-white text-[22px]"
+          />
+        )}
+      </div> */}
     </div>
   );
 }
